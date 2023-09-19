8dfb273862c40efce92bff0c43c7ad45f2eaec6d
Tarique Mosharraf <applandsys@gmail.com>
2023-09-19 23:59:39 +0600

    Home Page Design

---
+++ src/views/components/base/HomeLayout.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+
+import {IonContent, IonPage} from "@ionic/vue";
+</script>
+<template>
+    <ion-page>
+        <ion-content :fullscreen="true">
+            <div class="circle-top"></div>
+             <slot></slot>
+            <div class="circle-bottom"></div>
+        </ion-content>
+    </ion-page>
+</template>
+
+<style scoped>
+ion-content {
+    --background: var(--theme-background-color);
+}
+
+.circle-top
+{
+
+}
+
+.circle-bottom
+{
+
+}
+
+
+</style>
---
+++ src/views/pages/HomePage.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/vue';
+import { ref } from 'vue';
+import { playCircle } from 'ionicons/icons';
+import HomeLayout from "@/views/components/base/HomeLayout.vue";
+
+const myFirst = ref<string>('I fuck you Hima');
+
+const handleFuck = ()=>{
+  myFirst.value = "I fuck Hima";
+}
+</script>
+<template>
+   <HomeLayout>
+       <div id="container">
+           <div class="app-title"><h1>Mob Spec</h1></div>
+       </div>
+   </HomeLayout>
+</template>
+<style scoped>
+#container {
+  text-align: center;
+    .app-title{
+        & h1{
+            font-size: 2rem;
+            color: #744253;
+            font-family: fantasy;
+        }
+        & a {
+            text-decoration: none;
+        }
+    }
+}
+</style>
